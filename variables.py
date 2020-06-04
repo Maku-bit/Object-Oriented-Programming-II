53d981887e2a5683632e9d63f35adb6a8a01b408
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-04 08:13:18 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,33 @@
+#python variables
+#Variables are containers for storing data
+#To create a variable you assign using (=)
+#Examples
+x=3
+w="Alex"
+print (x)
+print(w)
+
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+#legal variable names
+myvar = "Alex"
+my_var = "Alex"
+_my_var = "Alex"
+myVar = "Alex"
+MYVAR = "Alex"
+myvar2 = "Alex"
+
+#Illegal variable names:
+#2myvar = "Alex"
+#my-var = "Alex"
+#my var = "Alex"
+
+
+#Assigning multiple Variables
+a,b,c = "Banana","Apple","Grapes"
+print(a)
+print(b)
+print(c)
