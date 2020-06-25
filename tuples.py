6c19db6622201e5eb35d379509d75df94c14b5ca
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-25 12:27:59 +0300

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,18 @@
+#A tuple is similar to a list but the only difference ist that you cant change values in tuples
+#A tuple is immutable
+#To set a tuple we use round brackets()
+tup=(30,35,45,10,6)
+print (tup)
+
+#To check index of a tuple we use square brackets
+tup[2]
+print (tup[2])
+
+#There ere only 2 methods in tuple;count and index to find index
+
+
+#sets
+#to create  a set we use {}
+
+s= {22,11,33,55,66,44}
+#sets dont follow the sequence in which elements are added
