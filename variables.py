f9a5fdd82bf9ddb5963c5d3f3bb931a88c544301
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-25 09:21:03 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -31,3 +31,32 @@ a,b,c = "Banana","Apple","Grapes"
 print(a)
 print(b)
 print(c)
+
+#In calculations underscore can be used to calculate values using a the last previous calculation result
+x=3
+y=4
+z=6
+x+y= 7
+_ +6 =13
+
+#To check the formula of a variable we use the len function
+myname= "Alexis"
+len (myname) =6
+
+#To slice you use indexing.The first index is [0]
+myname [1:4]= lex
+
+#When you use negative index it starts from the right side ie.
+len myname [-1] = s
+
+#Stirngs are imutable ie. Once you asign a variable with a string you can not change it
+
+
+
+
+
+
+
+
+
+
