56ba3d2d635d00f28da014591253e05aed176641
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-27 07:20:25 +0300

    Add files via upload

---
+++ numberconversion.py
@@ -0,0 +1,24 @@
+#you can change from bin to oct to hexadecimaalto decimal
+#to bin
+bin(21)
+print(bin(21))
+bin(65)
+print(bin(65))
+#to decimal we start with 0b
+0b110010
+print(0b110010)
+0b0101110010
+print(0b0101110010)
+#to octal
+oct(55)
+print(oct(55))
+print(oct(8))
+#to reverse oct we use 0o
+0o10
+print(0o10)
+#to change to hex
+hex(16)
+print(hex(15))
+print(hex(91))
+#to rexerse hex we use 0x
+print(0xd)
