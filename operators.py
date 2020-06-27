05d755cfdd298a053d4d33ce7d8ec660b443af64
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-27 07:19:31 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,44 @@
+#there are various types of operators;arithmetic operators,assignment operators,relational operators,logical operators,unary operators
+#Arithmetic operators
+x=4
+y=8
+print(x+y)
+print(y-x)
+print(x*y)
+print(x/y)
+#assignment operators
+w=2
+w=w+2
+print(w)
+#or
+w+=4
+print(w)
+w*=2
+print(w)
+
+#you can do assignment in one line
+x,y,d=2,5,6
+print(x,y,d)
+#relational operators
+#are called relational because you are trying to relate to different values
+y<d
+print(y<d)
+y=6
+print(y==d)
+x<=y
+print(x<=y)
+#Not equal to
+y!=d
+print(y<d)
+
+#to relate and check two conditions
+a=3
+b=5
+print((a<7)and(b<6))
+
+print((a<7)or(b>6))
+
+x=True
+print(x)
+print(not(x))
+x=not x
