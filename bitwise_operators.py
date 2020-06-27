5780a4c9a5a3610fb77af18521979e6eabf3bfa0
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-27 07:22:22 +0300

    Add files via upload

---
+++ bitwise_operators.py
@@ -0,0 +1,25 @@
+#bitwise operators include:Complement(~),And(&),Or(|),XOR(^),Left Shift(<<),Right Shift(>>)
+#complements
+~13
+print(~13)
+print(~121)
+#and(&) ,use truth table and binary to compare
+12&14
+print(12&14)
+print(35&71)
+#or(|)
+12|44
+print(12|44)
+print(22|13)
+#XOR -same values get 0 in truth tables
+12^15
+print(12^15)
+print(33^7)
+#left shift(<<)-gaining bits
+22<<3
+print(22<<3)
+print(14<<2)
+#Right shift(>>)-loosing bits
+65>>2
+print(65>>2)
+print(34>>3)
