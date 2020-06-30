0f90276feac99e2f1d0f198f5be1619299092164
makulolia <63190534+Maku-bit@users.noreply.github.com>
2020-06-30 10:11:00 +0300

    Add files via upload

---
+++ if_elif_statements.py
@@ -0,0 +1,26 @@
+#Used for flow of arguments
+#To check for even or odd number
+x=int(input("Enter your Number:"))
+z=x%2
+if z==0:
+    print('Your number is Even')
+else:
+    print("Your number is Odd")
+
+#You can have if in a if
+#Having multiple arguments
+#checking numbers 1-5
+
+w=int(input("Enter your Number:"))
+if w==1:
+    print('Number one')
+elif w==2:
+    print("Number2")
+elif w==3:
+    print("Number 3")
+elif w==4:
+    print("Number 4")
+elif w==5:
+    print("Number 5")
+else:
+    print("Incorrect input")
